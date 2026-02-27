138a96c127ce048c6953df122ed57c0d56715f73
arthur-tuko <arthurfranco502@gmail.com>
2026-02-27 08:36:24 -0300

    Add files via upload

---
+++ aula_02/exemplo06.php
@@ -0,0 +1,14 @@
+<?php
+    $a = 4;
+    $b = 0;
+
+    if ($b == 0){
+        echo "<p style = 'color: red;'>Erro </p>";
+    }else{
+        $r = $a / $b;
+        echo $r;
+    }
+
+    echo"<br>";
+    echo "Aqui continua sua programação...";
+?>
